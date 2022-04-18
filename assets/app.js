60240af0b658856e114a150a0e8580cadda74b7d
MathiasGenibrel <mathias.geni@gmail.com>
2022-04-18 17:27:38 +0200

    feat (login styles) add style to login route
    - Add multiple icon
    - Add multiple stealsheets
    - Add some pictures
    - modify structure of **login.html.twig**
    - Add Poppins font in base.html.twig

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,15 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import "./images/Background--Connection.png"
+
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/fontStyles.css";
+import "./styles/formConnection.css";
+
+// import "./styles/app.css";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";

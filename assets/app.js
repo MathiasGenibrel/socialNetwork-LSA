dfc642ac45a4c74aa9d98500bd04bbc1fb885713
Chis Centaure <c.centaure@webintelligence.fr>
2022-04-20 22:05:19 +0200

    fix error tittle

--- assets/app.js
+++ assets/app.js
@@ -8,20 +8,18 @@
 import "./images/Background--Connection.png"
 
 // any CSS you import will output into a single css file (app.css in this case)
-<<<<<<< HEAD
+
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/fontStyles.css";
 import "./styles/formConnection.css";
 
 // import "./styles/app.css";
-=======
+
 import './styles/app.scss';
-<<<<<<< HEAD
->>>>>>> main
-=======
+
 import './styles/post.css';
->>>>>>> main
+
 
 // start the Stimulus application
 import "./bootstrap";

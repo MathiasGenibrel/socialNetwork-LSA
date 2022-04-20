b261f50383926a51f423e9becc2f5330ebaae7f8
MathiasGenibrel <mathias.geni@gmail.com>
2022-04-20 19:58:28 +0200

    fix (css) fix xss of agree terms

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,37 +16,38 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, SocialNetworkLsaAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
-    {
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+  #[Route('/register', name: 'app_register')]
+  public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, SocialNetworkLsaAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+  {
+    $user = new User();
+    $form = $this->createForm(RegistrationFormType::class, $user);
+    $form->handleRequest($request);
 
-        $emailLastValue = $form->get('email')->getData();
+    $emailLastValue = $form->get('email')->getData();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+    if ($form->isSubmitted() && $form->isValid()) {
+      // encode the plain password
+      $user->setPassword(
+        $userPasswordHasher->hashPassword(
+          $user,
+          $form->get('plainPassword')->getData()
+        )
+      );
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+      $entityManager->persist($user);
+      $entityManager->flush();
+      // do anything else you need here, like send an email
 
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'emailLastValue' => $emailLastValue,
-        ]);
+      return $userAuthenticator->authenticateUser(
+        $user,
+        $authenticator,
+        $request
+      );
     }
+
+    return $this->render('registration/register.html.twig', [
+      'registrationForm' => $form->createView(),
+      'emailLastValue' => $emailLastValue,
+    ]);
+  }
 }
